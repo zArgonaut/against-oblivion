fc004ccb9b5489ffa2442045d9aaa060ade92899
zArgonaut <caioa1012@gmail.com>
2025-06-30 23:45:31 -0300

    Implement basic player systems

--- Assets/_Project/Scripts/Items/ItemPickup.cs
+++ Assets/_Project/Scripts/Items/ItemPickup.cs
@@ -1,8 +1,38 @@
-// Script de exemplo para ItemPickup.cs
 using UnityEngine;
+
+public enum TipoItem
+{
+    Pontos,
+    Vida,
+    Energia
+}
 
 public class ItemPickup : MonoBehaviour
 {
-    void Start() { }
-    void Update() { }
+    public TipoItem tipo;
+    public int valor = 10;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        var vida = other.GetComponent<PlayerHealth>();
+        var energia = other.GetComponent<PlayerEnergy>();
+
+        switch (tipo)
+        {
+            case TipoItem.Vida:
+                vida?.Heal(valor);
+                break;
+            case TipoItem.Energia:
+                energia?.Restore(valor);
+                break;
+            case TipoItem.Pontos:
+                if (ScoreManager.instance != null)
+                    ScoreManager.instance.AdicionarPontos(valor);
+                break;
+        }
+
+        Destroy(gameObject);
+    }
 }
